c5caf373e81455e5bc4d613d1ecc0f0510558ae9
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-03-31 23:59:29 +0700

    feat: inherit ERC721

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -3,9 +3,19 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract MyEpicGame {
-    // We'll hold our character's attributes in a struct. Feel free to add
-    // whatever you'd like as an attribute! (ex. defense, crit chance, etc).
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract MyEpicGame is ERC721 {
+    // The tokenId is the NFTs unique identifier, it's just a number that goes
+    // 0, 1, 2, 3, etc.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     struct CharacterAttributes {
         uint256 characterIndex;
         string name;
@@ -14,10 +24,18 @@ contract MyEpicGame {
         uint256 maxHp;
         uint256 attackDamage;
     }
+
     // A lil array to help us hold the default data for our characters.
     // This will be helpful when we mint new characters and need to know
     // things like their HP, AD, etc.
     CharacterAttributes[] defaultCharacters;
+
+    //  nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => the NFTs tokenId. Gives me an ez way
+    // to store the owner of the NFT and reference it later.
+    mapping(address => uint256) public nftHolders;
 
     // Data passed in to the contract when it's first created initializing the characters.
     // We're going to actually pass these values in from run.js.
@@ -26,7 +44,7 @@ contract MyEpicGame {
         string[] memory characterImageURIs,
         uint256[] memory characterHp,
         uint256[] memory characterAttackDmg
-    ) {
+    ) ERC721("Heroes", "HERO") {
         // Loop through all the characters, and save their values in our contract so
         // we can use them later when we mint our NFTs.
         for (uint256 i = 0; i < characterNames.length; i += 1) {
@@ -49,5 +67,6 @@ contract MyEpicGame {
                 c.imageURI
             );
         }
+        _tokenIds.increment();
     }
 }
