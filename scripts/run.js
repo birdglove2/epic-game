60e07688ec70adbdcd5c75c57ac1e62e23b33254
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-01 01:49:32 +0700

    Update run.js

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,7 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
   const gameContract = await gameContractFactory.deploy(
+    // Default characters
     ['Leo', 'Aang', 'Pikachu'],
     [
       'https://i.imgur.com/pKd5Sdk.png',
@@ -8,29 +9,27 @@ const main = async () => {
       'https://i.imgur.com/WMB6g9u.png',
     ],
     [100, 200, 300],
-    [100, 50, 25]
+    [100, 50, 25],
+
+    // Big Boss
+    'Elon Musk',
+    'https://i.imgur.com/AksR0tt.png',
+    10000,
+    50
   );
+
   await gameContract.deployed();
   console.log('Contract deployed to:', gameContract.address);
 
   let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log('Minted NFT #1');
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log('Minted NFT #2');
-
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
-  console.log('Minted NFT #3');
 
-  txn = await gameContract.mintCharacterNFT(1);
+  txn = await gameContract.attackBoss();
   await txn.wait();
-  console.log('Minted NFT #4');
 
-  console.log('Done deploying and minting!');
+  txn = await gameContract.attackBoss();
+  await txn.wait();
 };
 
 const runMain = async () => {
