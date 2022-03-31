51b4123068b7f1c652dced833b8a911b645f4696
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-01 01:38:40 +0700

    feat: getAllDefaultCharacters

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -227,4 +227,12 @@ contract MyEpicGame is ERC721 {
             return emptyStruct;
         }
     }
+
+    function getAllDefaultCharacters()
+        public
+        view
+        returns (CharacterAttributes[] memory)
+    {
+        return defaultCharacters;
+    }
 }
