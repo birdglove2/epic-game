4798c509c183d289c7b4223c583f7f929c3f259b
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-01 01:32:32 +0700

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,50 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+  const gameContract = await gameContractFactory.deploy(
+    // Default characters
+    ['Leo', 'Aang', 'Pikachu'],
+    [
+      'https://i.imgur.com/pKd5Sdk.png',
+      'https://i.imgur.com/xVu4vFL.png',
+      'https://i.imgur.com/WMB6g9u.png',
+    ],
+    [100, 200, 300],
+    [100, 50, 25],
+
+    // Big Boss
+    'Elon Musk',
+    'https://i.imgur.com/AksR0tt.png',
+    10000,
+    50
+  );
+
+  await gameContract.deployed();
+  console.log('Contract deployed to:', gameContract.address);
+
+  let txn;
+  txn = await gameContract.mintCharacterNFT(0);
+  await txn.wait();
+  console.log('Minted NFT #1');
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log('Minted NFT #2');
+
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+  console.log('Minted NFT #3');
+
+  console.log('Done deploying and minting!');
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+runMain();
