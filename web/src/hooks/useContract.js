afb31af9a969cdfab1b9865d243938797098c20b
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-04 00:10:14 +0700

    Update useContract.js

--- web/src/hooks/useContract.js
+++ web/src/hooks/useContract.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react';
 import { abi } from '../utils/MyEpicGame.json';
 import { ethers } from 'ethers';
 
 export const useContract = () => {
   const CONTRACT_ADDRESS = '0xECF765F923Ac195D7453Ff2C07d6b223239Fe6e8';
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const signer = provider.getSigner();
-  const contractInterface = new ethers.utils.Interface(abi);
-  const gameContract = new ethers.Contract(CONTRACT_ADDRESS, contractInterface, signer);
-  return { gameContract };
+  const [gameContract, setGameContract] = useState(null);
+
+  useEffect(() => {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
+
+      setGameContract(gameContract);
+    } else {
+      console.log('Ethereum object not found');
+    }
+  }, [window.ethereum]);
+
+  const transformCharacterData = (characterData) => {
+    return {
+      name: characterData.name,
+      imageURI: characterData.imageURI,
+      hp: characterData.hp.toNumber(),
+      maxHp: characterData.maxHp.toNumber(),
+      attackDamage: characterData.attackDamage.toNumber(),
+    };
+  };
+
+  return { gameContract, transformCharacterData };
 };
