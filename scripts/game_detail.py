d7acfd6046b9e57396d4233fbe6e03b06e96661e
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-16 16:35:32 +0700

    Create game_detail.py

---
+++ scripts/game_detail.py
@@ -0,0 +1,34 @@
+BOSS = (
+    "Elon Musk",
+    "https://i.imgur.com/AksR0tt.png",
+    10000,
+    10000,
+    50,
+)
+
+DEFAULT_CHARACTERS = [
+    (
+        0,
+        "Mega Charizard Y",
+        "bafybeiffh6fk2hujxi653zjnfe2gtj44ben2xvet5tu67ctc3m64e33c3m",
+        500,
+        500,
+        250,
+    ),
+    (
+        1,
+        "Lucario",
+        "QmV4ybsmVyUNdMDfks5qHFtPwiBMoY6iuoY9TPSTWsET5S",
+        400,
+        400,
+        250,
+    ),
+    (
+        2,
+        "Gengar",
+        "bafybeicvhxo2s5tk2336rsz2dys6blywigexsrgakyz75ynhgdbjo7rhea",
+        450,
+        450,
+        300,
+    ),
+]
