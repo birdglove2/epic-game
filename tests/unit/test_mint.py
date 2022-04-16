eeeea6fc012fb54b95794f852e66aec983642402
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-16 18:00:56 +0700

    refactor: test with variables

--- tests/unit/test_mint.py
+++ tests/unit/test_mint.py
@@ -1,6 +1,7 @@
 from brownie import network, exceptions
 from scripts.deploy_epic_game import deploy_epic_game
 from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.game_detail import DEFAULT_CHARACTERS
 import pytest
 
 
@@ -11,18 +12,19 @@ def test_user_can_mint_character():
 
     account = get_account()
 
-    tx = epic_game.mintCharacterNFT(1, {"from": account})
+    (index1, name1, imageURI1, hp1, maxHp1, attackDamage1) = DEFAULT_CHARACTERS[1]
+    tx = epic_game.mintCharacterNFT(index1, {"from": account})
     tx.wait(1)
 
     character_attributes = epic_game.getUserNFT({"from": account})
     (characterIndex, name, imageURI, hp, maxHp, attackDamage) = character_attributes
 
     assert characterIndex == 1
-    assert name == "Lucario"
-    assert imageURI == "QmV4ybsmVyUNdMDfks5qHFtPwiBMoY6iuoY9TPSTWsET5S"
-    assert hp == 400
-    assert maxHp == 400
-    assert attackDamage == 250
+    assert name == name1
+    assert imageURI == imageURI1
+    assert hp == hp1
+    assert maxHp == maxHp1
+    assert attackDamage == attackDamage1
 
 
 def test_user_cannot_mint_character_more_than_one():
