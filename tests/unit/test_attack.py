1f5c33cf4fb0f58b95679dfb64f44783e1c5efe9
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-16 18:16:03 +0700

    feat: reuse attack func & fix test_cannot_attack_with_dead_nft()

--- tests/unit/test_attack.py
+++ tests/unit/test_attack.py
@@ -1,4 +1,4 @@
-from brownie import network, exceptions
+from brownie import network, exceptions, EpicGame
 from scripts.deploy_epic_game import deploy_epic_game, deploy_and_mint
 from scripts.helpful_scripts import (
     get_account,
@@ -8,6 +8,22 @@ from scripts.helpful_scripts import (
 import pytest
 
 
+def attack(account, epic_game, random_number):
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+
+    attack_tx = epic_game.attackBoss({"from": account})
+    attack_tx.wait(1)
+    requestId = attack_tx.events["AttackInitiate"]["requestId"]
+
+    fulfill_attack_tx = get_contract("vrf_coordinator").callBackWithRandomness(
+        requestId, random_number, epic_game.address, {"from": account}
+    )
+    fulfill_attack_tx.wait(1)
+
+    return fulfill_attack_tx
+
+
 def test_attack_with_different_crit_chance():
     assert test_attack_with_crit_chance(777) == 7
     assert test_attack_with_crit_chance(999) == 9
@@ -15,20 +31,13 @@ def test_attack_with_different_crit_chance():
     assert test_attack_with_crit_chance(123) == 3
 
 
-def test_attack_with_crit_chance(random_number=100):
+def test_attack_with_crit_chance(random_number):
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip()
     account = get_account()
     epic_game = deploy_and_mint(account)
 
-    attack_tx = epic_game.attackBoss({"from": account})
-    attack_tx.wait(1)
-    requestId = attack_tx.events["AttackInitiate"]["requestId"]
-
-    fulfill_attack_tx = get_contract("vrf_coordinator").callBackWithRandomness(
-        requestId, random_number, epic_game.address, {"from": get_account()}
-    )
-    fulfill_attack_tx.wait(1)
+    fulfill_attack_tx = attack(account, epic_game, random_number)
     newBossHp = fulfill_attack_tx.events["AttackComplete"]["newBossHp"]
     newPlayerHp = fulfill_attack_tx.events["AttackComplete"]["newPlayerHp"]
 
@@ -41,9 +50,9 @@ def test_attack_with_crit_chance(random_number=100):
     assert critChance == random_number % 10
 
     damage = playerAttackDamage
-    if critChance >= 9:
+    if critChance >= 8:
         damage = playerAttackDamage * 3
-    elif 6 <= critChance <= 8:
+    elif 5 <= critChance <= 7:
         damage = playerAttackDamage * 2
 
     assert playerHp == newPlayerHp == playerMaxHp - bossAttackDamage
@@ -60,7 +69,7 @@ def test_cannot_attack_if_not_player():
         epic_game.attackBoss({"from": account})
 
 
-def test_dead_nft_cannot_attack():
+def test_cannot_attack_with_dead_nft():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip()
     account = get_account()
@@ -68,9 +77,11 @@ def test_dead_nft_cannot_attack():
 
     (_, _, _, _, playerMaxHp, _) = epic_game.getUserNFT({"from": account})
     (_, _, _, _, bossAttackDamage) = epic_game.getBigBoss()
+
+    # how many times player can attack before die
     count = playerMaxHp // bossAttackDamage
     for _ in range(count):
-        epic_game.attackBoss({"from": account})
+        attack(account, epic_game, 0)
 
     (_, _, _, playerHp, _, _) = epic_game.getUserNFT({"from": account})
     assert playerHp == 0
