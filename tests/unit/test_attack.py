01904d9706f5fdcb878bd24ef5815727e28e184b
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-16 17:51:13 +0700

    feat: attack with 0 crit

--- tests/unit/test_attack.py
+++ tests/unit/test_attack.py
@@ -1,6 +1,10 @@
 from brownie import network, exceptions
 from scripts.deploy_epic_game import deploy_epic_game, deploy_and_mint
-from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.helpful_scripts import (
+    get_account,
+    get_contract,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 import pytest
 
 
@@ -10,15 +14,25 @@ def test_attack_successfully():
     account = get_account()
     epic_game = deploy_and_mint(account)
 
-    epic_game.attackBoss({"from": account})
+    attack_tx = epic_game.attackBoss({"from": account})
+    attack_tx.wait(1)
+    requestId = attack_tx.events["AttackInitiate"]["requestId"]
+
+    fulfill_attack_tx = get_contract("vrf_coordinator").callBackWithRandomness(
+        requestId, 4, epic_game.address, {"from": get_account()}
+    )
+    fulfill_attack_tx.wait(1)
+    # print("call trace", fulfill_attack_tx.call_trace())
+    newBossHp = fulfill_attack_tx.events["AttackComplete"]["newBossHp"]
+    newPlayerHp = fulfill_attack_tx.events["AttackComplete"]["newPlayerHp"]
 
     (_, _, _, playerHp, playerMaxHp, playerAttackDamage) = epic_game.getUserNFT(
         {"from": account}
     )
     (_, _, bossHp, bossMaxHp, bossAttackDamage) = epic_game.getBigBoss()
-
-    assert playerHp == playerMaxHp - bossAttackDamage
-    assert bossHp == bossMaxHp - playerAttackDamage
+    assert playerHp == newPlayerHp == playerMaxHp - bossAttackDamage
+    assert bossHp == newBossHp == bossMaxHp - playerAttackDamage
+    assert epic_game.critChance() == 4
 
 
 def test_cannot_attack_if_not_player():
