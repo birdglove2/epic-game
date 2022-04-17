3d67750a9b50ab7c9c580811c61c97587ff51ac0
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-17 23:38:46 +0700

    feat: add github repo link

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import twitterLogo from 'assets/twitter-logo.svg';
+import githubLogo from 'assets/GitHub-Mark-32px.png';
 import './App.css';
 import SelectCharacter from 'Components/SelectCharacter';
 import LoadingIndicator from 'Components/LoadingIndicator';
@@ -9,6 +10,7 @@ import { useContract, useAccount } from 'hooks';
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const GITHUB_LINK = 'https://github.com/birdglove2/turn-based-nft-card-game';
 
 const App = () => {
   const [characterNFT, setCharacterNFT] = useState(null);
@@ -75,14 +77,26 @@ const App = () => {
         </div>
       </div>
 
-      <div className="flex justify-center items-center">
-        <img alt="Twitter Logo" className="w-9 h-9" src={twitterLogo} />
+      <div className="flex flex-col justify-center items-center">
         <a
-          className="text-sm font-bold underline"
-          href={TWITTER_LINK}
           target="_blank"
-          rel="noreferrer"
-        >{`built with @${TWITTER_HANDLE}`}</a>
+          rel="noopener noreferrer"
+          href={GITHUB_LINK}
+          className="flex justify-center items-center"
+        >
+          <img alt="Github Logo" className="w-9 h-9 mr-2" src={githubLogo} />
+          <p className="text-sm font-bold">visit the code</p>
+        </a>
+
+        <div className="flex justify-center items-center">
+          <img alt="Twitter Logo" className="w-9 h-9" src={twitterLogo} />
+          <a
+            className="text-sm font-bold"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noopener noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
       </div>
     </div>
   );
