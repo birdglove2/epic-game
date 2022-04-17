568b5c054e248573ff64242fefcaba0ac1b035cf
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-04-17 19:37:13 +0700

    fix: false function

--- scripts/attack.py
+++ scripts/attack.py
@@ -2,7 +2,7 @@ from brownie import EpicGame, network
 from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 
 
-def attack(epic_game=None, account=get_account):
+def attack(epic_game=None, account=get_account()):
     attack_tx = epic_game.attackBoss({"from": account})
     attack_tx.wait(1)
     newPlayerHp = attack_tx.events["AttackComplete"]["newPlayerHp"]
@@ -10,7 +10,7 @@ def attack(epic_game=None, account=get_account):
     return newPlayerHp, newBossHp
 
 
-def attack_til_boss_dead(epic_game=None, account=get_account):
+def attack_til_boss_dead(epic_game=None, account=get_account()):
     (_, _, bossHp, _, _) = epic_game.getBigBoss()
     while bossHp > 0:
         playerHp, bossHp = attack(epic_game, account)
@@ -18,7 +18,7 @@ def attack_til_boss_dead(epic_game=None, account=get_account):
             epic_game.revive({"from": account})
 
 
-def attack_til_player_dead(epic_game=None, account=get_account):
+def attack_til_player_dead(epic_game=None, account=get_account()):
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         epic_game = EpicGame[-1]
     else:
